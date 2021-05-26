e4789b6dfcfcfd18e4ad2986d87eecc346d6dfd2
swethasadanandan <76676443+swethasadanandan@users.noreply.github.com>
2021-05-26 17:26:13 +0530

    Add files via upload

---
+++ AA.java
@@ -0,0 +1,4 @@
+package pack;
+public class AA{
+public void msg(){System.out.println("HELLO");}
+}
---
+++ BB.java
@@ -0,0 +1,8 @@
+package mypack;
+import pack.*;
+class BB{
+public static void main(String args[]){
+AA obj = new AA();
+obj.msg();
+}
+}
