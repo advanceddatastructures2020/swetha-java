79772263571d8df15c1b36e6c15db8e113dcbbf8
swethasadanandan <76676443+swethasadanandan@users.noreply.github.com>
2021-08-31 13:41:45 +0530

    Add files via upload

---
+++ Gcd.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Gcd {
+
+    public static void main(String[] args) {
+
+        int num1, num2;
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        num1 = (int)scanner.nextInt();
+        
+        System.out.print("Enter second number:");
+        num2 = (int)scanner.nextInt();
+        
+        scanner.close();
+        while (num1 != num2) {
+        	if(num1 > num2)
+                num1 = num1 - num2;
+            else
+                num2 = num2 - num1;
+        }
+
+        System.out.printf("GCD of given numbers is: %d", num2);
+    }
+
+}
+© 2021 GitHub, Inc.
